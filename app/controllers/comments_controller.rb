c969c0299c3b000250cff665d3668265ffdfb46d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-01 13:52:45 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
     comment = Comment.create(comment_params)
     post = Post.find_by(id: comment_params[:post_id])
     post.comments << comment
-    raise post.comments.inspect
     redirect_to comment.post
   end
 
