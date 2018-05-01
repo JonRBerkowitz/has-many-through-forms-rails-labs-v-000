7d9553fb8dac1903f63e5477d1863e7b7e318e24
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-01 14:07:06 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
+    raise comment_params/inspect
     comment = Comment.create(comment_params)
     post = Post.find_by(id: comment_params[:post_id])
     post.comments << comment
     user = User.find_by(id: comment_params[:user_id])
+    if !user
+      user = User.create(username: => comment_params)
+    end
     user.comments << comment
     redirect_to comment.post
   end
