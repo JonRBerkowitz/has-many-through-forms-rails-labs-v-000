09a4e9edc1951cb74e4607be163aa813ceb0b9e7
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-01 14:29:54 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,9 @@ class CommentsController < ApplicationController
     post = Post.find_by(id: comment_params[:post_id])
     post.comments << comment
     user = User.find_by(id: comment_params[:user_id])
-
+    if !user
+      user = User.create(:username => a[:username])
+    end
     user.comments << comment
     redirect_to comment.post
   end
