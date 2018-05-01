f5264730b329273def874f6b4021f949fc9c5a4c
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-01 14:29:58 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
 
   def create
     a = comment_params[:user_attributes]
-    raise a[:username].inspect
     comment = Comment.create(comment_params)
     post = Post.find_by(id: comment_params[:post_id])
     post.comments << comment
