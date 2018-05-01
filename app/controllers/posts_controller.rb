fdc9f98bb3b5617ee5c2baeb3d4054a73e431fa6
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-05-01 12:58:49 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
-    @post.comments.build
+    @comment = Comment.new
   end
 
   def index
@@ -10,7 +10,6 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    @post.categories.build(name: 'Enter a name')
   end
 
   def create
@@ -18,16 +17,9 @@ class PostsController < ApplicationController
     redirect_to post
   end
 
-  def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    redirect_to post
-  end
-
-
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comments_attributes: [:content], users_attributes: [:username])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
